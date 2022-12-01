ce5af4860011764803bfbd0b015fa04a80af2eeb
Michael Timblin <mjtimblin@gmail.com>
2022-12-01 16:16:21 -0600

    Refactor day 1 2022 to use the updated template and better variable names

--- 2022/Day 1/solution.py
+++ 2022/Day 1/solution.py
@@ -1,27 +1,31 @@
 import os.path
+from typing import List
 
 
-def get_inventories(dataset=[]):
+def get_inventories(dataset: List[str]):
     inventories = []
     while '' in dataset:
-        inventories.append([int(l) for l in dataset[:dataset.index('')]])
+        inventories.append([int(item) for item in dataset[:dataset.index('')]])
         dataset = dataset[dataset.index('') + 1:]
-    inventories.append([int(l) for l in dataset])
+    inventories.append([int(item) for item in dataset])
     return inventories
 
 
-def part_1(dataset=[]):
+def part_1(dataset: List[str]) -> str:
     inventories = get_inventories(dataset)
-    return max([sum(i) for i in inventories])
+    sorted_inventory_sums = sorted([sum(i) for i in inventories])
+    return str(max(sorted_inventory_sums))
 
 
-def part_2(dataset=[]):
+def part_2(dataset: List[str]) -> str:
     inventories = get_inventories(dataset)
-    return sum(sorted([sum(i) for i in inventories])[-3:])
+    sorted_inventory_sums = sorted([sum(i) for i in inventories])
+    return str(sum(sorted_inventory_sums[-3:]))
 
 
+# noinspection DuplicatedCode
 def run_tests():
-    test_data = [l.strip() for l in '''
+    test_data = [line.strip() for line in '''
 1000
 2000
 3000
@@ -37,20 +41,30 @@ def run_tests():
 
 10000
 '''.strip().splitlines()]
-    expected_part_1 = 24000
-    expected_part_2 = 45000
+    expected_part_1 = '24000'
+    expected_part_2 = '45000'
+    actual_part_1 = part_1(test_data)
+    actual_part_2 = part_2(test_data)
 
-    assert expected_part_1 == part_1(test_data)
-    assert expected_part_2 == part_2(test_data)
+    test_num = 1
+    try:
+        assert expected_part_1 == actual_part_1
+        test_num = 2
+        assert expected_part_2 == actual_part_2
+    except AssertionError:
+        print(f'Part {test_num} test failed. Expected {expected_part_1 if test_num == 1 else expected_part_2}, got {actual_part_1 if test_num == 1 else actual_part_2}')
+        exit(1)
+
     print('Tests passed!')
 
 
+# noinspection DuplicatedCode
 def main():
     run_tests()
 
     filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'input.txt')
     f = open(filepath, 'r')
-    input_lines = [l.strip() for l in f.readlines()]
+    input_lines = [line.strip() for line in f.readlines()]
     f.close()
 
     print(f'Part 1 solution: {part_1(input_lines)}')
