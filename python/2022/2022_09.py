ece56868a8932a0e23d893d540fa6a525a5c6c07
Michael Timblin <mjtimblin@gmail.com>
2022-12-12 11:11:58 -0600

    Update solution for 2022 day 9 with working part 2

--- python/2022/2022_09.py
+++ python/2022/2022_09.py
@@ -8,29 +8,47 @@ class Solution(BaseSolution):
         data_prefix = '2022_09'
         super().__init__(use_test_data, data_prefix)
 
-    def _get_tail_location_for_movement(self, new_head: Tuple[int, int], tail_location: Tuple[int, int], direction: str) -> Tuple[int, int]:
-        if abs(new_head[0] - tail_location[0]) > 1:
-            if new_head[0] > tail_location[0]:
-                new_tail = (tail_location[0] + 1, new_head[1])
-            else:
-                new_tail = (tail_location[0] - 1, new_head[1])
-        elif abs(new_head[1] - tail_location[1]) > 1:
-            if new_head[1] > tail_location[1]:
-                new_tail = (new_head[0], tail_location[1] + 1)
-            else:
-                new_tail = (new_head[0], tail_location[1] - 1)
-        elif new_head[0] == tail_location[0] and abs(new_head[1] - tail_location[1]) > 1:
-            if new_head[1] > tail_location[1]:
-                new_tail = (tail_location[0], tail_location[1] + 1)
-            else:
-                new_tail = (tail_location[0], tail_location[1] - 1)
-        elif new_head[1] == tail_location[1] and abs(new_head[0] - tail_location[0]) > 1:
-            if new_head[0] > tail_location[0]:
-                new_tail = (tail_location[0] + 1, tail_location[1])
-            else:
-                new_tail = (tail_location[0] - 1, tail_location[1])
-        else:
-            new_tail = tail_location
+    def _get_tail_location_for_movement(self, head_location: Tuple[int, int], tail_location: Tuple[int, int]) -> Tuple[int, int]:
+        new_tail = (tail_location[0], tail_location[1])
+        # Tail needs to move down
+        if head_location[0] > tail_location[0] + 1:
+            new_tail = (tail_location[0] + 1, tail_location[1])
+
+            # Check to see if tail needs to move diagonally either right or left
+            if new_tail[1] < head_location[1]:
+                new_tail = (new_tail[0], new_tail[1] + 1)
+            elif new_tail[1] > head_location[1]:
+                new_tail = (new_tail[0], new_tail[1] - 1)
+
+        # Tail needs to move up
+        elif head_location[0] + 1 < tail_location[0]:
+            new_tail = (tail_location[0] - 1, tail_location[1])
+
+            # Check to see if tail needs to move diagonally either right or left
+            if new_tail[1] < head_location[1]:
+                new_tail = (new_tail[0], new_tail[1] + 1)
+            elif new_tail[1] > head_location[1]:
+                new_tail = (new_tail[0], new_tail[1] - 1)
+
+        # Tail needs to move right
+        if head_location[1] > tail_location[1] + 1:
+            new_tail = (tail_location[0], tail_location[1] + 1)
+
+            # Check to see if tail needs to move diagonally either up or down
+            if new_tail[0] < head_location[0]:
+                new_tail = (new_tail[0] + 1, new_tail[1])
+            elif new_tail[0] > head_location[0]:
+                new_tail = (new_tail[0] - 1, new_tail[1])
+
+        # Tail needs to move left
+        elif head_location[1] + 1 < tail_location[1]:
+            new_tail = (tail_location[0], tail_location[1] - 1)
+
+            # Check to see if tail needs to move diagonally either up or down
+            if new_tail[0] < head_location[0]:
+                new_tail = (new_tail[0] + 1, new_tail[1])
+            elif new_tail[0] > head_location[0]:
+                new_tail = (new_tail[0] - 1, new_tail[1])
 
         return new_tail
 
@@ -50,7 +68,7 @@ class Solution(BaseSolution):
                     current_head = (current_head[0] - 1, current_head[1])
                 else:
                     current_head = (current_head[0] + 1, current_head[1])
-                current_tail = self._get_tail_location_for_movement(current_head, current_tail, direction)
+                current_tail = self._get_tail_location_for_movement(current_head, current_tail)
                 tail_locations.add(current_tail)
 
         return str(len(tail_locations))
@@ -63,16 +81,16 @@ class Solution(BaseSolution):
             distance = int(distance)
             for _ in range(distance):
                 if direction == 'U':
-                    knots[0] = (knots[0][0], knots[0][1] + 1)
-                elif direction == 'D':
                     knots[0] = (knots[0][0], knots[0][1] - 1)
+                elif direction == 'D':
+                    knots[0] = (knots[0][0], knots[0][1] + 1)
                 elif direction == 'L':
                     knots[0] = (knots[0][0] - 1, knots[0][1])
                 else:
                     knots[0] = (knots[0][0] + 1, knots[0][1])
 
                 for i in range(1, len(knots)):
-                    knots[i] = self._get_tail_location_for_movement(knots[i - 1], knots[i], direction)
+                    knots[i] = self._get_tail_location_for_movement(knots[i - 1], knots[i])
                     if i == 9:
                         tail_locations.add(knots[i])
 
